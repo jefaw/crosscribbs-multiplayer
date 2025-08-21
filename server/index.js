4899d88a0d267bb33a1f1a5ae65d31b6b0f23cc4
Jeffrey Wang <jeffrey.19jw@gmail.com>
2025-08-20 20:32:12 -0400

    some changes to game test interface so it actually uses the gamecontroller. Still needs lots of tests

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(__dirname));
 
 const server = http.createServer(app);
