d497ea1a6d3617717226641c0afe6bdef9c03836
BenDaBeast22 <bencritoph@gmail.com>
2025-08-27 01:11:57 -0400

    fix visual shift

--- app/ui/Game/Player.tsx
+++ app/ui/Game/Player.tsx
@@ -49,37 +49,38 @@ export default function Player({ name, player, turn, crib, numPlayers }: ChildPr
   // Only show card if it's the player's turn
   const displayCard =
     isActive && card ? (
-      <div className="flex flex-col items-center space-y-2">
+      <div className="flex flex-col items-center space-y-2 h-66">
         <img
-          className="w-36 h-51 self-center hover:border-gray-700 border-transparent border-2 cursor-pointer rounded-lg shadow-lg transition-transform hover:scale-105"
+          className="w-32 h-45 self-center hover:border-gray-700 border-transparent border-2 cursor-pointer rounded-lg shadow-lg transition-transform hover:scale-105"
           src={card.frontImgSrc}
           alt=""
           draggable={true}
           onDragStart={handleDragStart}
         />
         <p className="text-base font-medium text-gray-700">Cards remaining: {hand.length}</p>
+
         {displayDiscardButton && (
           <button
             onClick={handleDiscard}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-2"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
           >
             Discard to Crib
           </button>
         )}
       </div>
     ) : (
-      <div className="flex flex-col items-center space-y-2">
-        <img className="w-36 h-51 self-center rounded-lg shadow-lg" src={backImgSrc} alt="" draggable={false} />
-        <p className="text-base font-medium text-gray-700 h-5">Cards remaining: {hand.length}</p>
+      <div className="flex flex-col items-center space-y-2 h-66">
+        <img className="w-32 h-45 self-center rounded-lg shadow-lg" src={backImgSrc} alt="" draggable={false} />
+        <p className="text-base font-medium text-gray-700">Cards remaining: {hand.length}</p>
       </div>
     );
 
-  const noCard = <div className="h-58"></div>; // card height: 51 + p height: 5 + y-spcaing: 2
+  const noCard = <div className="h-66"></div>; // card height: 51 + p height: 5 + y-spcaing: 2
 
   return (
     <>
       <div
-        className={`flex flex-col justify-center ${bgGradient} m-8 py-6 px-4 rounded-lg ${outlineStyle} transition-all duration-300 shadow-xl backdrop-blur-sm`}
+        className={`flex flex-col justify-center ${bgGradient} m-8 py-4 px-4 rounded-lg ${outlineStyle} transition-all duration-300 shadow-xl backdrop-blur-sm`}
       >
         <h1 className="text-center text-xl font-bold mb-3 text-gray-800">{name}</h1>
         {card ? displayCard : noCard}
