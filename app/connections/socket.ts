81ec57eed07f62cc0b4d6bbff5166ef66e07739e
BenDaBeast22 <bencritoph@gmail.com>
2025-09-17 12:27:22 -0400

    update socket for prod

--- app/connections/socket.ts
+++ app/connections/socket.ts
@@ -1,7 +1,8 @@
 // client/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000");
+// export const socket = io("http://localhost:4000");
+export const socket = io(import.meta.env.VITE_BACKEND_URL || window.location.origin);
 
 // optional: log connection once
 socket.on("connect", () => console.log("Socket connected:", socket.id));
